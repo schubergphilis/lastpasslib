8330d47cebd25996ebc1c29747408b4a1b0e0d55
Costas Tyfoxylos <costas.tyf@gmail.com>
2023-02-02 19:43:37 +0100

    Extend mock appropriatelly. Document isolated testing of decryption.

--- lastpass/utils.py
+++ lastpass/utils.py
@@ -1,3 +1,6 @@
+from .datamodels import SharedFolder
+
+
 class LastpassMock:
 
     def __init__(self, username, domain='lastpass.com'):
@@ -5,3 +8,28 @@ class LastpassMock:
         self.host = f'https://{domain}'
         self.username = username
         self.iteration_count = 100100
+
+    def get_shared_folder_by_id(self, id_):
+        params = ['x' for _ in range(16)]
+        params.insert(0, id_)
+        return SharedFolder(*params)
+
+# Decryption code can be tested in isolation by applying it to downloaded vault blob.
+# A vault blob can be saved from a proper authenticated session like:
+#
+# from lastpass.lastpass import Lastpass
+# lastpass = Lastpass(USERNAME, PASSWORD, MFA)
+# with open('vault.blob') as ofile:
+#     ofile.write(lastpass.vault._blob.decode('utf-8'))
+
+# and can be loaded to test the decryption code in isolation like:
+#
+# from lastpass.utils import LastpassMock
+# from lastpass.entities import Vault
+#
+# lastpass = LastpassMock(CORRECT_USERNAME)
+# vault = Vault(lastpass, CORRECT_PASSWORD)
+# with open('vault.blob') as ifile:
+#     data = ifile.read().encode('utf-8')
+#
+# secrets = vault._decrypt_blob(data)
